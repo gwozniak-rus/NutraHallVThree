1accd6760fc90aa56a1acce048608f70315fa29d
Greg Wozniak <gregorywozniak2020@gmail.com>
2023-04-25 11:25:46 -0400

    UI finished, working on the timer function in this branch

--- app/src/main/java/com/example/nutrahallvthree/MainActivity3.java
+++ app/src/main/java/com/example/nutrahallvthree/MainActivity3.java
@@ -12,30 +12,31 @@ import java.util.Stack;
 
 public class MainActivity3 extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-
         final Random randomCode = new Random();
-
         Button generateButton = (Button) findViewById(R.id.codeGenerate);
-
         final TextView textGenerate = (TextView) findViewById(R.id.generated);
+
+
+
         generateButton.setOnClickListener(new View.OnClickListener() {
-                                              @Override
-                                              public void onClick(View v) {
-                                                  //this will create the random code when the random code button is clicked
-                                                  textGenerate.setText(String.valueOf(randomCode.nextInt(10000)));
+            @Override
+            public void onClick(View v) {
+                //this will create the random code when the random code button is clicked
+                textGenerate.setText(String.valueOf(randomCode.nextInt(10000)));
+            }
 
-                                              }
 
-                                              //need something to start the timer on this application
 
-                                              //then, will complete the time loop and send to database
-                                              //total time will be compiled at end of week and shown to user
-                                          }
-        );
-    }
-}
+            //this is where the timer will start and stop.
+            //will have a send button that will send the time to database
+                                          });
+
+
+
+}}
--- app/src/main/java/com/example/nutrahallvthree/retrofit/RetrofitService.java
+++ app/src/main/java/com/example/nutrahallvthree/retrofit/RetrofitService.java
@@ -17,7 +17,7 @@ public class RetrofitService {
     private void initializeRetrofit(){
         //Retrofit builder = new Retrofit.Builder()
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.4.56:8080/")
+                .baseUrl("http://172.28.3.173:8080/")
                 .addConverterFactory(GsonConverterFactory.create(new Gson()))
                 //.client(new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build())
                 .build();
